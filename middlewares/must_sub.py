4e709214e070480b7607c7a4ee4d8b4476ea427f
Cyber-coder-dot <shoxruxraxmonqulov006@gmail.com>
2025-04-15 12:13:21 +0500

    middle wares ga fayl qoshildi

---
+++ middlewares/must_sub.py
@@ -0,0 +1,5 @@
+from aiogram import types
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from data.config import CHANNELS
+from loader import bot
+from aiogram.dispatcher.handler import CancelHandler
